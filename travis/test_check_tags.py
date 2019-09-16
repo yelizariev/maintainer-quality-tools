5b44ee4a1fda5e5f4f54a89e3fa04c3e0672eff6
kaadevelop <oleg88rubakov@gmail.com>
2019-09-16 18:44:19 +0500

    check tags check versions of docs
    check tags check versions of docs

--- travis/test_check_tags.py
+++ travis/test_check_tags.py
@@ -10,14 +10,16 @@ version = os.environ.get('VERSION', '')
 token = os.environ.get('GITHUB_TOKEN')
 travis_pull_request_number = os.environ.get('TRAVIS_PULL_REQUEST')
 travis_repo_slug = os.environ.get('TRAVIS_REPO_SLUG')
+travis_pr_slug = os.environ.get('TRAVIS_PULL_REQUEST_SLUG')
 travis_branch = os.environ.get('TRAVIS_BRANCH')
+travis_build_dir = os.environ.get('TRAVIS_BUILD_DIR')
 error_msg = "Check guidelines: https://gitlab.com/itpp/handbook/blob/master/documenting-updates.md If you are not IT-Projects' employee, you can ignore it and we'll handle it by our own"
 exit_status = 0
-result = check_tags.get_errors_msgs_commits(travis_repo_slug, travis_pull_request_number, travis_branch, version, token)
+result = check_tags.get_errors_msgs_commits(travis_repo_slug, travis_pull_request_number, travis_branch, version, token, travis_build_dir, travis_pr_slug)
 count_errors = len(result.keys())
 if count_errors > 0:
     for key, value in result.items():
-        print('Wrong commit:')
+        print('Wrong commit or changed of files:')
         print(travis_helpers.yellow("{commit}".format(commit=key)))
         print(travis_helpers.red("{errors}".format(errors=value)))
         print(error_msg)
